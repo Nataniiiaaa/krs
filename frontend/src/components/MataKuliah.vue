e01c57ee0e6a74b6145c92a1d3102ee500a24832
Natania Setyawati <natania.nn64@gmail.com>
2024-01-03 14:23:52 +0800

    sorting based on semester

--- frontend/src/components/MataKuliah.vue
+++ frontend/src/components/MataKuliah.vue
@@ -104,7 +104,12 @@ export default {
       axios.get(url).then(({ data }) => {
         console.log(data);
         this.allMatakuliah = data;
+        this.sortMatakuliah(); // Panggil fungsi sort setelah memuat data
       });
+    },
+    sortMatakuliah() {
+      // Fungsi untuk menyortir array matakuliah berdasarkan semester
+      this.allMatakuliah.sort((a, b) => a.semester - b.semester);
     },
     removeMatakuliah(Matakuliah) {
       var url = `https://api-group7-prognet.manpits.xyz/api/matakuliah/${Matakuliah.id}`;
